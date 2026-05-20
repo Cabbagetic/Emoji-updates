2dc59486ae381e37edf81bda8e12f5a1c3911303
Esterajisi <go.pu.ten10@gmail.com>
2026-05-20 23:08:22 +0530

    Update emoji variables and print statements

---
+++ New-emojis/Emoji-updates5.java
@@ -0,0 +1,44 @@
+public class Emoji_updates_of_this_discord_server { 
+    public static void main(String[] args) { 
+        String emoji13 = ":cabbagetic:";
+        String emoji11 = ":cabbage1_by_gemini:";
+        String emoji12 = ":cabbage2_by_gemini:";
+        String emoji9 = ":Sitting_MC_Red_parrot:";
+        String emoji8 = ":Sitting_MC_Green_parrot:";
+        String emoji7 = ":Sitting_MC_Gray_parrot:";
+        String emoji5 = ":Sitting_MC_Blue_parrot:";
+        String emoji6 = ":Sitting_MC_Cyan_parrot:";
+        String emoji3 = ":Server_Renovation:";
+        String emoji4 = ":Server_Renovation_blur:";
+        String emoji10 = ":absolutelycinema:";
+        String emoji1 = ":2Hi:";
+        String emoji17 = ":golden_dandelion1:";
+        String emoji2 = ":MC:";
+        String emoji18 = ":mc:";
+        String emoji14 = ":cabbagetic2:";
+        String emoji15 = ":enderman:";
+        String emoji16 = ":endermanpeak:";
+        String emoji19 = ":pi3dot1415926535897932384626433:";
+        String emoji20 = ":vr:";
+        System.out.println("#1 Emoji Name is " + emoji1);
+        System.out.println("#2 Emoji Name is " + emoji2);
+        System.out.println("#3 Emoji Name is " + emoji3);
+        System.out.println("#4 Emoji Name is " + emoji4);
+        System.out.println("#5 Emoji Name is " + emoji5);
+        System.out.println("#6 Emoji Name is " + emoji6);
+        System.out.println("#7 Emoji Name is " + emoji7);
+        System.out.println("#8 Emoji Name is " + emoji8);
+        System.out.println("#9 Emoji Name is " + emoji9);
+        System.out.println("#10 Emoji Name is " + emoji10);
+        System.out.println("#11 Emoji Name is " + emoji11);
+        System.out.println("#12 Emoji Name is " + emoji12);
+        System.out.println("#13 Emoji Name is " + emoji13);
+        System.out.println("#14 Emoji Name is " + emoji14);
+        System.out.println("#15 Emoji Name is " + emoji15);
+        System.out.println("#16 Emoji Name is " + emoji16);
+        System.out.println("#17 Emoji Name is " + emoji17);
+        System.out.println("#18 Emoji Name is " + emoji18);
+        System.out.println("#19 Emoji Name is " + emoji19);
+        System.out.println("#20 Emoji Name is " + emoji20);
+    }
+}
